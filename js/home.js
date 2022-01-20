adb3f800ee32a2019bce7591408df39f989e1277
khein21501 <khein21501@gmail.com>
2022-01-20 19:25:51 +0630

    index

--- js/home.js
+++ js/home.js
@@ -1,16 +1,9 @@
-function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+
+function table_NO() {
+    let tableNumber = document.querySelector("#tableno").value;
+    console.log(tableNumber);
+    if (tableNumber>0) {
+        
     }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
-  
-  // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
+    
+}
